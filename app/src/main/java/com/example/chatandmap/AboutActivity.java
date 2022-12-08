2a80917afcbfd86b804c8345d83396125b3e801f
Малыгин Стас <s.maligin939@gmail.com>
2022-12-08 23:07:11 +0300

    11.07 08.12.2022

---
+++ app/src/main/java/com/example/chatandmap/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.example.chatandmap;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+}
