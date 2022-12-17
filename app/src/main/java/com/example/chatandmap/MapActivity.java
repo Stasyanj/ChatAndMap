f9cf47453b59e643ea9b5e763247cfc10416967c
Малыгин Стас <s.maligin939@gmail.com>
2022-12-18 01:20:46 +0300

    Get Maps!!!!

--- app/src/main/java/com/example/chatandmap/MapActivity.java
+++ app/src/main/java/com/example/chatandmap/MapActivity.java
@@ -1,17 +1,36 @@
 package com.example.chatandmap;
 
+import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.chatandmap.databinding.MapActivityBinding;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private MapActivityBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.map_activity);
+        binding = MapActivityBinding.inflate(getLayoutInflater());
+       // setContentView(R.layout.map_activity);
+        setContentView(binding.getRoot());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager() .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         ImageButton exit_map_button = (ImageButton) findViewById(R.id.imageButton4);
         Intent myIntent = new Intent(MapActivity.this, MainActivity.class);
         //Тут код карты
@@ -21,6 +40,13 @@ public class MapActivity extends AppCompatActivity {
                 MapActivity.this.startActivity(myIntent);
             }
         });
+    }
+    public void onMapReady(GoogleMap googleMap){
+        this.mMap = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
     public void onBackPressed(){
         Intent exit_intent = new Intent(MapActivity.this, MainActivity.class);
